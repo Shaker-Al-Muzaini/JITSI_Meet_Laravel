68f7d15eed02cdeba5e12c8b843bea97d60f05d2
Shaker Al Muzaini <92122042+Shaker-Al-Muzaini@users.noreply.github.com>
2023-01-15 09:33:21 +0200

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Providers;
+namespace App\Providers; 
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
